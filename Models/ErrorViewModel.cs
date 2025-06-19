987fb69d69de4079cbb2af0c1a7739a442a6f81b
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-06-20 01:15:43 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Realestate_ERP_Dashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
